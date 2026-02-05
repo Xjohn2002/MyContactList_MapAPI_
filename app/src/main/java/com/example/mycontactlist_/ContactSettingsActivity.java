ec7c0b2dca8d34dc3f7ed36b23a0a3a5c0b0b1ae
Xjohn2002 <xjohnson3@student.gsu.edu>
2026-02-05 12:29:21 -0500

    First commit

---
+++ app/src/main/java/com/example/mycontactlist_/ContactSettingsActivity.java
@@ -0,0 +1,127 @@
+package com.example.mycontactlist_;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ContactSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contact_settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+
+    }
+    private void initListButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initMapButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setEnabled(false);
+    }
+    private void initSettings(){
+        String sortBy = getSharedPreferences("MyContactListPreferences",
+                MODE_PRIVATE).getString("sortfield","contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences",
+                MODE_PRIVATE).getString("sortorder","ASC");
+
+        RadioButton rbName = findViewById(R.id.radioName);
+        RadioButton rbCity = findViewById(R.id.radioCity);
+        RadioButton rbBirthday = findViewById(R.id.radioBirthday);
+        if (sortBy.equalsIgnoreCase("contactname")){
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("city")){
+            rbCity.setChecked(true);
+        }
+        else {
+            rbBirthday.setChecked(true);
+        }
+        RadioButton rbAscending = findViewById(R.id.radioAscending);
+        RadioButton rbDescending = findViewById(R.id.radioDescending);
+        if (sortBy.equalsIgnoreCase("ASC")){
+            rbAscending.setChecked(true);
+        }
+        else {
+            rbDescending.setChecked(true);
+        }
+    }
+    private void initSortByClick() {
+        RadioGroup rgSortBy = findViewById(R.id.radioGroupSortBy);
+        rgSortBy.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                RadioButton rbName = findViewById(R.id.radioName);
+                RadioButton rbCity = findViewById(R.id.radioCity);
+                if (rbName.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            MODE_PRIVATE).edit()
+                            .putString("sortfield", "contactname").apply();
+                } else if (rbCity.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            MODE_PRIVATE).edit()
+                            .putString("sortfield", "city").apply();
+                } else {
+                    getSharedPreferences("MyContactListPreferences",
+                            MODE_PRIVATE).edit()
+                            .putString("sortfield", "birthday").apply();
+                }
+            }
+        });
+    }
+    private void initSortOrderClick(){
+        RadioGroup rgSortOrder = findViewById(R.id.radioGroupSortOrder);
+        rgSortOrder.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                RadioButton rbAscending = findViewById(R.id.radioAscending);
+                if (rbAscending.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            MODE_PRIVATE).edit()
+                            .putString("sortorder","ASC").apply();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences",
+                            MODE_PRIVATE).edit()
+                            .putString("sortorder", "DESC").apply();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/mycontactlist_/MainActivity.java
@@ -0,0 +1,230 @@
+package com.example.mycontactlist_;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Contact currentContact;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+        initToggleButton();
+        setForEditing(false);
+        initChangeDateButton();
+        initSaveButton();
+        initTextChangedEvents();
+
+        currentContact = new Contact();
+    }
+
+    private void initListButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initMapButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(MainActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initSettingsButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(MainActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initToggleButton(){
+        final ToggleButton editToggle = findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                setForEditing(editToggle.isChecked());
+
+            }
+        });
+    }
+    private void  setForEditing(boolean enabled){
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipCode = findViewById(R.id.editZipCode);
+        EditText editHome = findViewById(R.id.editHome);
+        EditText editCell = findViewById(R.id.editCell);
+        EditText editEmail = findViewById(R.id.editEMail);
+        Button buttonChange = findViewById(R.id.btnBirthday);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipCode.setEnabled(enabled);
+        editHome.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+
+        if (enabled){
+            editName.requestFocus();
+        }
+        else{
+            ScrollView s = findViewById(R.id.scrollView);
+            s.fullScroll(ScrollView.FOCUS_UP);
+        }
+    }
+    private void initChangeDateButton(){
+        Button changeDate = findViewById(R.id.btnBirthday);
+        changeDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                FragmentManager fm =getSupportFragmentManager();
+                DatePickerDialog datePickerDialog = new DatePickerDialog();
+                datePickerDialog.show(fm,"DatePick");
+            }
+        });
+
+    }
+    //  maybe Temporary
+    private void initSaveButton(){
+        Button saveButton=  findViewById(R.id.buttonSave);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                hideKeyboard();
+                boolean wasSuccessful;
+
+                ContactDataSource ds = new ContactDataSource(MainActivity.this);
+                try{
+                    ds.open();
+                    if (currentContact.getContactID()==-1) {
+                        wasSuccessful = ds.insertContact(currentContact);
+                        int newId = ds.getLastContactID();
+                        currentContact.setContactID(newId);
+                    }
+                    else {
+                        wasSuccessful = ds.updateContact(currentContact);
+                    }
+                    ds.close();
+                } catch (Exception e) {
+                    wasSuccessful = false;
+                }
+                if (wasSuccessful){
+                    ToggleButton editToggle = findViewById(R.id.toggleButtonEdit);
+                    editToggle.toggle();
+                    setForEditing(false);
+                }
+            }
+        });
+    }
+    private void hideKeyboard(){
+        InputMethodManager imm= (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        EditText editName =findViewById(R.id.editName);
+        imm.hideSoftInputFromWindow(editName.getWindowToken(), 0);
+        EditText editAddress =findViewById(R.id.editAddress);
+        imm.hideSoftInputFromWindow(editAddress.getWindowToken(), 0);
+        EditText editCity =findViewById(R.id.editCity);
+        imm.hideSoftInputFromWindow(editCity.getWindowToken(), 0);
+        EditText editState =findViewById(R.id.editState);
+        imm.hideSoftInputFromWindow(editState.getWindowToken(), 0);
+        EditText editZipCode =findViewById(R.id.editZipCode);
+        imm.hideSoftInputFromWindow(editZipCode.getWindowToken(), 0);
+        EditText editHome =findViewById(R.id.editHome);
+        imm.hideSoftInputFromWindow(editHome.getWindowToken(), 0);
+        EditText editCell =findViewById(R.id.editCell);
+        imm.hideSoftInputFromWindow(editCell.getWindowToken(), 0);
+        EditText editEmail =findViewById(R.id.editEMail);
+        imm.hideSoftInputFromWindow(editEmail.getWindowToken(), 0);
+
+    }
+    private void initTextChangedEvents() {
+        final EditText etContactName = findViewById(R.id.editName);
+        etContactName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setContactName(etContactName.getText().toString());
+
+            }
+        });
+        final EditText etStreetAddress = findViewById(R.id.editAddress);
+        etStreetAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setStreetAddress(etContactName.getText().toString());
+
+            }
+        });
+        final EditText etHomePhone = findViewById(R.id.editHome);
+        etHomePhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        final EditText etCellPhone = findViewById(R.id.editCell);
+        etCellPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+    }
+}
