f77672c65480579e43c9abc219b1974432b471dc
Xjohn2002 <xjohnson3@student.gsu.edu>
2026-02-05 14:07:00 -0500

    added getContactName code (Listing 6.1)

--- app/src/main/java/com/example/mycontactlist_/ContactDataSource.java
+++ app/src/main/java/com/example/mycontactlist_/ContactDataSource.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 public class ContactDataSource {
 
     private SQLiteDatabase database;
@@ -78,6 +80,23 @@ public class ContactDataSource {
             //do nothing - will return false if there is an exception
         }
         return didSucceed;
+    }
+    public ArrayList<String> getContactName(){
+        ArrayList<String>contactNames = new ArrayList<>();
+        try{
+            String query = "Select contactname from contact";
+            Cursor cursor = database.rawQuery(query,null);
+
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()) {
+                contactNames.add(cursor.getString(0));
+                cursor.close();
+            }
+            catch (Exception e){
+                contactNames = new ArrayList<String>();
+            }
+            return  contactNames;
+        }
     }
 
 
