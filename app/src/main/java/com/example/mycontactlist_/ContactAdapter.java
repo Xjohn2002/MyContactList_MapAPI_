b46373b11ceca964d7cae2d1ee5d4488a732c2ac
Xjohn2002 <xjohnson3@student.gsu.edu>
2026-02-10 13:29:58 -0500

    modified Adapter code for new layout Listing 6.14

--- app/src/main/java/com/example/mycontactlist_/ContactAdapter.java
+++ app/src/main/java/com/example/mycontactlist_/ContactAdapter.java
@@ -1,10 +1,12 @@
 package com.example.mycontactlist_;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,18 +17,25 @@ import java.util.ArrayList;
 //Listing 6.2
 //Modified code in Listing 6.4
 //Modified code in Listing 6.7 for new layout
+//Modified code in Listing 6.14 for new layout
 public class ContactAdapter extends RecyclerView.Adapter {
     private ArrayList<Contact> contactData;
     private View.OnClickListener mOnItemClickListener;
-    // is "mOnItemClickListener" a typo?
+    private boolean isDeleting;
+    private Context parentContext;
 
-    public class ContactViewHolder extends RecyclerView.ViewHolder {
+    public ContactAdapter(ArrayList<Contact> arrayList, Context context){
+        contactData = arrayList;
+        parentContext = context;
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder{
 
         public TextView textViewContact;
         public TextView textPhone;
         public Button deleteButton;
 
-        public ContactViewHolder(@NonNull View itemView) {
+        public ContactViewHolder(@NonNull View itemView){
             super(itemView);
             textViewContact = itemView.findViewById(R.id.textViewName);
             textPhone = itemView.findViewById(R.id.textPhoneNumber);
@@ -35,43 +44,64 @@ public class ContactAdapter extends RecyclerView.Adapter {
             itemView.setOnClickListener(mOnItemClickListener);
         }
         public TextView getPhoneTextView(){
-
             return textPhone;
         }
         public Button getDeleteButton(){
-
             return deleteButton;
         }
-
-    public TextView getContactTextView() {
+        public TextView getContactTextView(){
             return textViewContact;
+
         }
     }
     public ContactAdapter(ArrayList<String> arrayList){
-
         contactData = arrayList;
-    }
-    public void setOnItemClickListener(View.OnClickListener itemClickListener){
-        mOnItemClickListener = itemClickListener;
     }
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_item_view,
-                parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View v = LayoutInflater.from(parent.getContext()) .inflate(R.layout.simple_item_view,
+                parent,false);
         return new ContactViewHolder(v);
     }
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position){
         ContactViewHolder cvh = (ContactViewHolder) holder;
         cvh.getContactTextView().setText(contactData.get(position).getContactName());
         cvh.getPhoneTextView().setText(contactData.get(position).getPhoneNumber);
+        if (isDeleting){
+            cvh.getDeleteButton().setVisibility(View.VISIBLE);
+            cvh.getDeleteButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteItem(position);
+                }
+            });
+        }
+        else{
+            cvh.getDeleteButton().setVisibility(View.INVISIBLE);
+        }
+    }
+    public void setDelete(boolean b){
+        isDeleting = b;
+    }
+    private void deleteItem(int position){
+        Contact contact = contactData.get(position);
+        ContactDataSource ds = new ContactDataSource(parentContext);
+        try{
+            ds.open();
+            boolean didDelete = ds.deleteContact(contact.getContactID());
+            ds.close();
+            if (didDelete){
+                contactData.remove(position);
+                notifyDataSetChanged();
+            }
+            else{
+                Toast.makeText(parentContext,"Delete Failed!",Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(parentContext,"Delete Failed!",Toast.LENGTH_LONG).show();
+        }
 
     }
-    @Override
-    public int getItemCount(){
-        return contactData.size();
-    }
-
-
 }
